5c67ae1c9f5055e080cc56504aac4d115b1d32c9
oriafo <abrahamibharunujele@gmail.com>
2022-06-13 05:18:01 -0700

    functionalities fully implemented

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ options = ["R", "P", "S"]
 
 
 def rock_paper_scissors_game():
+    max_score = 0
     cpu_Score = 0
     player_Score = 0 
     status = True
-    while status == True:
+    print("Input the winning score:")
+    winning_point = int(input())
+
+
+    while max_score < winning_point:
         cpu = random.choice(options)
         User_Input = input("Enter your choice...:")
         if User_Input == "R" or User_Input == "P" or User_Input == "S":
@@ -26,21 +31,19 @@ def rock_paper_scissors_game():
                 player_Score += 1
             elif cpu == User_Input: 
                 print("Its a tie")
-            else:
-                print("nothing")
-            status = not(status)
         else:
-            ##print("CPU({}) : Player({})".format(cpu, User_Input))
             print("Invalid input.. Please input another value")
-            #status = not(status)
-        
+        if cpu_Score > player_Score:
+            max_score = cpu_Score
+        elif cpu_Score < player_Score:
+            max_score = player_Score
+
+    print("player: {}, while cpu: {}".format(player_Score, cpu_Score))
     if cpu_Score > player_Score:
         print("winner is: CPU")
     elif cpu_Score < player_Score:
         print("winner is: Player")
-    #else:
-        #print("its a tie")
-
-#the loop is not working
+    elif cpu_Score == player_Score:
+        print("its a tie")
 
 rock_paper_scissors_game()   
